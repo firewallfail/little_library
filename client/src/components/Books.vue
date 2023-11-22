2de05e166fac656db2c5dc23a2d3994be2f9e443
firewallfail <firewall.fail@hotmail.com>
2023-11-21 22:14:05 -0500

    add book sorting

--- client/src/components/Books.vue
+++ client/src/components/Books.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="scanner">
+    <label for="sort">Sort by: </label> 
+    <select v-model="sort_choice" @change="on_sort_change($event)">
+      <option v-for="sort in sort_options" v-bind:values="{id: sort.id, text: sort.text}">{{ sort.text }}</option>
+    </select>
     <div v-if="books_found" v-for="book of book_list">
       <BookCard :book="book" />
     </div>
@@ -8,19 +12,25 @@
   
 <script>
   import BookCard from '@/components/BookCard.vue'
-  
+
   export default {
     name: "Books",
     components: {
       BookCard
     },
     data(){
-        return {
-            book_list: [],
-            books_found: false,
-            offset: 0,
-            end_of_local: false
-        }
+      return {
+        sort_choice: '',
+        sort_options: [
+          {id: 1, text: 'Title', Title: 'title'},
+          {id: 2, text: 'Published', Published: 'published_date'},
+          {id: 3, text: 'Author', Author: 'authors'},
+        ],
+        book_list: [],
+        books_found: false,
+        offset: 0,
+        end_of_local: false
+      }
     },
     methods: {
       async get_local_books() {
@@ -45,6 +55,25 @@
         if (bookSearchResponse.data.length < 50) {
             this.end_of_local = true
         }
+      },
+      on_sort_change(event) {
+        const sort_strategy = {
+          Title: 'title',
+          Author: 'authors',
+          Published: 'published_date'
+        }
+        this.book_list.sort((a, b) => {
+          const sort_method = sort_strategy[event.target.value]
+          const val_a = a[sort_method]
+          const val_b = b[sort_method]
+          if (val_a < val_b) {
+            return -1
+          }
+          if (val_a > val_b) {
+            return 1
+          }
+          return 0
+        })
       }
     },
     mounted() {
